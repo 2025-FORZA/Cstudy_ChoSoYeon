b8e51fc4b8bed830bab1b76dc650685444d58030
soyeon101 <soyeoncho100@@sookmyung.ac.kr>
2025-04-06 18:00:39 +0900

    1_Lab02

---
+++ 1_Lab02_1.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	long long a, b, c;
+	//printf("a,b,c를 입력하시오: ");
+	scanf("%lld %lld %lld", &a, &b, &c);
+	printf("%lld\n", (a + b) % c);
+	printf("%lld\n", ((a % c) + (b % c)) % c);
+	printf("%lld\n", (a * b) % c);
+	printf("%lld\n", ((a % c) * (b % c)) % c);
+
+	return 0;
+}

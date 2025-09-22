c2ce35041381c37f93a140709e0255a339b66a76
soyeon101 <soyeoncho100@sookmyung.ac.kr>
2025-09-23 00:04:31 +0900

    2_Lab03

---
+++ 2_Lab03_01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+	printf("|\\_/|\n");
+	printf("|q p|   /}");
+	printf("\n");
+	printf("( 0 )\"\"\"\\");
+	printf("\n");
+	printf("|\"^\"\`    |\n");
+	printf("||_/=\\\\__|\n");
+
+
+}
